b886eea5aded2ef26a95c7a22bc40d1c520f6c60
Zichen Yang <36917866+RazzyChen@users.noreply.github.com>
2025-08-14 09:50:04 +0800

    Update __init__.py

--- model/utils/__init__.py
+++ model/utils/__init__.py
@@ -1,11 +1,10 @@
 # Model utilities
 
-from .ActivationFunction import SwiGLU, swiglu
+from .ActivationFunction import SwiGLU
 from .RoPE import RotaryEmbedding, RotaryPositionalEmbedding, apply_rotary_pos_emb, rotate_half
 
 __all__ = [
     "SwiGLU",
-    "swiglu", 
     "RotaryEmbedding",
     "RotaryPositionalEmbedding",
     "apply_rotary_pos_emb",
